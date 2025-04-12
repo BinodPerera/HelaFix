5a49d6920eac9fc6d3197742b65ad3c098f1e69d
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-04-12 12:37:31 +0530

    job one page done

--- helafix_mobile_app/lib/pages/job-done.dart
+++ helafix_mobile_app/lib/pages/job-done.dart
@@ -11,22 +11,20 @@ class JobDone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    int currentStep = 1; // This is a static value for now, modify it as needed.
+    int currentStep = 1;
 
     return Scaffold(
       appBar: CustomAppBar(),
-      backgroundColor: themeProvider.isDarkMode ? AppColours.primaryDark : AppColours.primaryLight,
+      backgroundColor: themeProvider.isDarkMode? AppColours.primaryDark: AppColours.primaryLight,
       body: Container(
         padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
-          gradient: themeProvider.isDarkMode ? AppColours.backgroundGradientDark : AppColours.backgroundGradientLight,
+          gradient: themeProvider.isDarkMode? AppColours.backgroundGradientDark: AppColours.backgroundGradientLight,
         ),
-        child: SingleChildScrollView(
           child: Column(
             children: [
-              
               Container(
-                padding: EdgeInsets.all(30),
+                padding: EdgeInsets.all(70),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -53,9 +51,61 @@ class JobDone extends StatelessWidget {
                   ],
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(40),
+                child: Text(
+                  'Service Provider Confirmation',
+                  style: TextStyle(
+                    color: themeProvider.isDarkMode
+                        ? AppColours.dark
+                        : AppColours.light,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                child: Text(
+                  'Waiting for service provider response.....',
+                  style: TextStyle(
+                    color: themeProvider.isDarkMode
+                        ? AppColours.dark
+                        : AppColours.light,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                child: CircularProgressIndicator(),
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 0, 0, 0), backgroundColor: const Color.fromRGBO(217, 217, 217, 1), 
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Button was clicked!')),
+                    );
+                  },
+                  child: Text('Special Inquiries',
+                  style: TextStyle(
+                    color: themeProvider.isDarkMode ? AppColours.dark : AppColours.light,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                ),
+              ),
             ],
           ),
-        ),
       ),
       bottomNavigationBar: CustomBottomNavBar(
         onItemTapped: (index) {
