307d495b853311f90bbc44e057020c0417d6679b
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-04-12 11:20:35 +0530

    path function done

--- helafix_mobile_app/lib/pages/job-done.dart
+++ helafix_mobile_app/lib/pages/job-done.dart
@@ -5,100 +5,67 @@ import '../theme_provider.dart';
 import '../theme/colors.dart';
 import 'package:helafix_mobile_app/components/appbar.dart';
 
-class JobDone extends StatefulWidget {
-  const JobDone({super.key});
-
-  @override
-  State<JobDone> createState() => _JobDoneState();
-}
-
-class _JobDoneState extends State<JobDone> {
-  int currentStep = 1;
+class JobDone extends StatelessWidget {
+  const JobDone({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
+    int currentStep = 1; // This is a static value for now, modify it as needed.
+
     return Scaffold(
       appBar: CustomAppBar(),
+      backgroundColor: themeProvider.isDarkMode ? AppColours.primaryDark : AppColours.primaryLight,
       body: Container(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
           gradient: themeProvider.isDarkMode ? AppColours.backgroundGradientDark : AppColours.backgroundGradientLight,
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(3, (index) {
-            int step = index + 1;
-            bool isVisited = step < currentStep;
-            bool isCurrent = step == currentStep;
-
-            return Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: isCurrent ? Colors.black : Colors.grey,
-                      width: 2,
-                    ),
-                  ),
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundColor: isVisited
-                        ? const Color.fromARGB(255, 0, 255, 8)
-                        : Colors.white,
-                    child: Text(
-                      '$step',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              
+              Container(
+                padding: EdgeInsets.all(30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 1; i <= 3; i++) ...[
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor:
+                            i < currentStep ? Colors.green : Colors.white,
+                        child: Text(
+                          '$i',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                      if (i != 3)
+                        Container(
+                          width: 40,
+                          height: 2,
+                          color: Colors.black,
+                        ),
+                    ]
+                  ],
                 ),
-                if (step != 3)
-                  Container(
-                    width: 40,
-                    height: 2,
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                  ),
-              ],
-            );
-          }),
+              ),
+            ],
+          ),
         ),
-        
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          if (currentStep > 1)
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  currentStep--;
-                });
-              },
-              child: Icon(Icons.arrow_back),
-            ),
-          SizedBox(width: 10),
-          if (currentStep < 3)
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  currentStep++;
-                });
-              },
-              child: Icon(Icons.arrow_forward),
-            ),
-        ],
+      bottomNavigationBar: CustomBottomNavBar(
+        onItemTapped: (index) {
+          if (index == 0) {
+            Navigator.pushNamed(context, '/home');
+          } else if (index == 1) {
+            Navigator.pushNamed(context, '/bookmarks');
+          }
+        },
       ),
-      bottomNavigationBar: CustomBottomNavBar(onItemTapped: (index) {
-        if (index == 0) {
-          Navigator.pushNamed(context, '/home');
-        } else if (index == 1) {
-          Navigator.pushNamed(context, '/bookmarks');
-        }
-      }),
     );
   }
 }
